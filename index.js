223d1f0d757121487c26221341380ba0a51e757e
fandyajpo <fandyglitch3@gmail.com>
2023-03-26 13:04:46 +0800

    fix manbrodsa

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
+require("dotenv").config();
+const cors = require("@koa/cors");
+const { koaBody } = require("koa-body");
+
+const Koa = require("koa");
+const Router = require("koa-router");
+
+const app = new Koa();
+
+app.use(cors());
+app.use(koaBody());
+
+const Public = new Router();
+app.use(Public.routes());
+app.use(Public.allowedMethods());
+
+app.on("error", (err, ctx) => {
+  console.log({ hasError: "I guess yes", status: err, route: ctx });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`App Lister to ${process.env.PORT}`);
+});
+
+export default async function Fan(ctx) {
+  try {
+    ctx.response.status = 200;
+    return (ctx.body = {
+      message: "Alive",
+    });
+  } catch (err) {
+    ctx.response.status = 400;
+    return (ctx.body = { status: ctx.status, message: e.message });
+  }
+}
+
+Public.get("/", Fan);
+
 const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-// HAI
 
 client.login(process.env.DISCORD_TOKEN);
-
-export default function handler(req, res) {
-  res.status(200).end("Hello Bot");
-}
